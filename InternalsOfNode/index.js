177e59dea806d0c0663eb4afecf9fc4f2be59f5c
DavidLapadula <lapadula.david@gmail.com>
2025-03-23 22:39:08 -0400

    [InternalsOfNode] testing node threads

---
+++ InternalsOfNode/index.js
@@ -0,0 +1,11 @@
+const crypto = require('crypto');
+
+const start = Date.now();
+
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+    console.log('1:', Date.now() - start); // how long it takes to run function
+});
+
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+    console.log('2:', Date.now() - start); 
+});
