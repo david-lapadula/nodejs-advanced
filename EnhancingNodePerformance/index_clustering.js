67150fafed7fdde7442ab83b5101c4d384b12fa5
DavidLapadula <lapadula.david@gmail.com>
2025-04-09 21:41:17 -0400

    [EnhancingNodePerformance] update comments

--- EnhancingNodePerformance/index_clustering.js
+++ EnhancingNodePerformance/index_clustering.js
@@ -8,12 +8,13 @@ if (cluster.isMaster) {
     // cause index.js to be executed *again* in child mode
     // add more .fork() for more instances
     // Every child will have won threadpool, unless modified by UV_THREADPOOL_SIZE = 1, meaning every child only has 1 thread
+    // If children > cores system will context switch and slow down overall performance
     cluster.fork();
 } else {
     // Child mode, just act like a server
 
     app.get('/', (req, res) => {
-        // simulate serer doing work
+        // simulate server doing work
         crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
             res.send('Normal');
         })
