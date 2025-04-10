87fbdecd19cf0200986e5fd15e34e1f85a5174b8
DavidLapadula <lapadula.david@gmail.com>
2025-04-09 21:37:58 -0400

    [EnhancingNodePerformance] add small test using worker threads

--- EnhancingNodePerformance/index.js
+++ EnhancingNodePerformance/index.js
@@ -1,28 +1,41 @@
 const express = require('express');
 const cluster = require('cluster');
+const crypto = require('crypto');
+const Worker = require('webworker-threads').Worker;
 const app = express();
 
-// Is the file being executed in master mode?
-if (cluster.isMaster) {
-    // cause index.js to be executed *again* in child mode
-    // add more .fork() for more instances
-    cluster.fork();
-} else {
-    // Child mode, just act like a server
-    function doWork(duration) {
-        const start = Date.now();
-        while (Date.now() - start < duration) {}
+
+app.get('/', (req, res) => {
+
+    // Function passed in gets stringified and passed to library. Cannot refer to outside program variables.
+    const worker = new Worker(function() {
+        // need function keywword to refer to right context and not method context
+        this.onmessage = function() {
+            let counter = 0;
+
+            while (counter < 1e9) {
+                counter++;
+            }
+
+            postMessage(counter);
+        }
+    });
+
+    // Post message back to application
+    worker.onmessage = function(message) {
+       console.log(message.data);
+       res.send(''+  message.data); // express assumes a status code when you send a number
     }
 
-    app.get('/', (req, res) => {
-        doWork();
-        res.send('Normal');
-    });
+    // Push data into thread
+    worker.postmessage = function() {
 
-    app.get('/fast', (req, res) => {
-        res.send('Fast');
-    });
-    
-    
-    app.listen(3000);
-}
+    }
+});
+
+app.get('/fast', (req, res) => {
+    res.send('Fast');
+});
+
+
+app.listen(3000);
---
+++ EnhancingNodePerformance/index_manual.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cluster = require('cluster');
+const crypto = require('crypto');
+const app = express();
+
+// Is the file being executed in master mode?
+if (cluster.isMaster) {
+    // cause index.js to be executed *again* in child mode
+    // add more .fork() for more instances
+    // Every child will have won threadpool, unless modified by UV_THREADPOOL_SIZE = 1, meaning every child only has 1 thread
+    cluster.fork();
+} else {
+    // Child mode, just act like a server
+
+    app.get('/', (req, res) => {
+        // simulate serer doing work
+        crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+            res.send('Normal');
+        })
+    });
+
+    app.get('/fast', (req, res) => {
+        res.send('Fast');
+    });
+
+
+    app.listen(3000);
+}
